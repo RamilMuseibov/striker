80cca4280508c451877426a48008036c89df4c57
RamilMuseibov <Ramilmys@gmail.com>
2025-07-11 19:56:33 +0700

    редачим

---
+++ flat.js
@@ -0,0 +1,11 @@
+//на вход [[1,2],[3,4]]
+//на выходе [1,2,3,4]
+// не мутировать исходникик
+
+function flat(arr) {
+  let i = 0;
+  while (i < arr.length) {
+    console.log(arr[i]);
+  }
+}
+flat();
--- sort.js
+++ sort.js
@@ -1,13 +1,5 @@
-const arr = [5, 7, 9, 2, 1];
+const arr = [5, 7, 9, 2, 1, 7, 5, 7];
 const arr2 = [5, 2, 8, 1, 4];
-
-// function sortArr(arr) {
-//   for (let i = 0; i < arr.length; i++) {
-//     let minNum = arr[0];
-//     if(minNum > arr[i]){
-//         minNum = arr[i]
-//     }
-//     console.log(minNum);
 
 //   }
 // }
@@ -19,25 +11,67 @@ const arr2 = [5, 2, 8, 1, 4];
 // ставим после предыдущего
 //
 
-function findMin(arr) {
+function sorting(arr) {
   let i = 0;
-  let minNum = arr[0];
-  let minIndex = 0;
-  const elem1 = arr[0];
 
-  while (i < arr.length) {
-    if (minNum > arr[i]) {
-      minNum = arr[i];
-      minIndex = i;
+  while (i < arr.length - 1) {
+    let j = i;
+    let minNum = arr[i];
+    let minIndex = i;
+
+    while (j < arr.length) {
+      if (minNum > arr[j]) {
+        minNum = arr[j];
+        minIndex = j;
+      }
+      j++;
     }
+
+    let rightElem = arr[i];
+    arr[i] = minNum;
+    arr[minIndex] = rightElem;
+
     i++;
   }
   return arr;
 }
-console.log(findMin(arr));
+// console.log(sorting(arr));
 
-// const firstElem = arr[0];
-// arr[0] = arr[1];
-// arr[1] = firstElem;
+function sort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minNum = arr[i];
+    let minIndex = i;
 
+    for (let j = i; j < arr.length; j++) {
+      if (minNum > arr[j]) {
+        minNum = arr[j];
+        minIndex = j;
+      }
+    }
+
+    let rightElem = arr[i];
+    arr[i] = minNum;
+    arr[minIndex] = rightElem;
+  }
+}
+// sort(arr);
 // console.log(arr);
+
+function toSorted(arr) {
+  const arrB = [...arr];
+  // let arrB = structuredClone(arr);
+  // let arrB = JSON.parse(JSON.stringify(arr));
+  // const arrB = arr.map((value) => value);
+  sort(arrB);
+  return arrB;
+}
+// console.log(toSorted(arr), arr);
+
+function calculator(a, b, isSum) {
+  if (isSum === true) {
+    return a - b;
+  } else {
+    return a + b;
+  }
+}
+console.log(calculator(5, 7, true));
