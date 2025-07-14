c87a00b9eb6536adb5da3394cc060af098c3d209
RamilMuseibov <Ramilmys@gmail.com>
2025-07-15 04:14:23 +0700

    Задача решена

--- shuffle.js
+++ shuffle.js
@@ -1,21 +1,20 @@
-let arr = [1, 2, 3, 4, 5]; //на входе массив
+// let arr = [1, 2, 3, 4, 5]; //на входе массив
 // let arr = [1, 2, 4, 7, 9, 13, 78, 123];
+let arr = [1, 1, 2, 2, 3, 4, 5];
 // на выходе массив [3,5,1,4,2] - в случайном порядке
 
 function shuffle(arr) {
-  let newArr = [];
-  let currentIndex = arr.length;
   let i = 0;
   while (i < arr.length) {
+    let currentIndex = arr.length;
     let randomIndex = Math.floor(Math.random() * currentIndex);
-    if (!newArr.includes(arr[randomIndex])) {
-      // console.log("Все нормально");
-      newArr.push(arr[randomIndex]);
-      i++;
-    }
-    // console.log("А тут пошло по пизде");
+
+    let randomItem = arr[randomIndex];
+    arr[randomIndex] = arr[i];
+    arr[i] = randomItem;
+    i++;
   }
-  return newArr;
+  return arr;
 }
 console.log(shuffle(arr));
-// 
+//
