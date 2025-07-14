1c635def0d7df3d3e200a93905d1ad1b273d07d2
RamilMuseibov <Ramilmys@gmail.com>
2025-07-15 00:41:35 +0700

    Задача готова

--- sample.js
+++ sample.js
@@ -1,16 +1,12 @@
 // на вход [чего угодно]
 // на выход случайный элемент этого  массива
 
-let arr = [1, 2, 3, 4, 5]; //на входе массив
-// // на выходе массив [3,5,1,4,2] - в случайном порядке..
+// let arr = [1, 2, 3, 4, 5]; //на входе массив
+let arr = [7, 10, 2, 14];
 
 function sample(arr) {
-  let i = 0;
-  let currentIndex = arr.length + 1;
-  while (i < arr.length) {
-    randomElement = Math.floor(Math.random() * currentIndex);
-    i++;
-  }
-  return randomElement;
+  let currentIndex = arr.length;
+  let randomIndex = Math.floor(Math.random() * currentIndex);
+  return arr[randomIndex];
 }
 console.log(sample(arr));
