8f1f6356a96ea0072b454e873beec09b7d34265e
RamilMuseibov <Ramilmys@gmail.com>
2025-07-14 21:17:20 +0700

    готово

--- sample.js
+++ sample.js
@@ -1,3 +1,16 @@
 // на вход [чего угодно]
 // на выход случайный элемент этого  массива
 
+let arr = [1, 2, 3, 4, 5]; //на входе массив
+// // на выходе массив [3,5,1,4,2] - в случайном порядке
+
+function sample(arr) {
+  let i = 0;
+  let currentIndex = arr.length + 1;
+  while (i < arr.length) {
+    randomElement = Math.floor(Math.random() * currentIndex);
+    i++;
+  }
+  return randomElement;
+}
+console.log(sample(arr));
