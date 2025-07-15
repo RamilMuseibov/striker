e82112ea96f2b26dbce2938cb7143d1d4537560f
RamilMuseibov <Ramilmys@gmail.com>
2025-07-16 03:05:00 +0700

    Здача решена

--- omit.js
+++ omit.js
@@ -1,6 +1,19 @@
 const input = { foo: "bar", a: 1, b: 2 };
-const paths = ["a"];
+const pathss = ["a"];
 
-function omit(obj, paths) {} // omit исключит paths
+// function omit(obj, paths) {} // omit исключит paths
 
 // {foo: "bar", b: 2} - на выходее
+
+function omit(obj, paths) {
+  obj = { ...input };
+  let i = 0;
+  while (i < paths.length) {
+    if (paths[i] in obj) {
+      delete obj[paths[i]];
+    }
+    i++;
+  }
+  return obj;
+}
+console.log(omit(input, pathss));
