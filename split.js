65c5600cbb1fde44d68cc43d99862a112f77e0e8
RamilMuseibov <Ramilmys@gmail.com>
2025-09-07 21:02:29 +0700

    Задача решена

---
+++ split.js
@@ -0,0 +1,21 @@
+const str =
+  "строка такая, прям берёшь и копируешь это сообщение и посмотрим, что выведет, на это сообщение";
+
+function split(str, splitter) {
+  let i = 0;
+  let token = "";
+  const result = [];
+  while (i < str.length) {
+    if (str[i] === splitter) {
+      result.push(token);
+      token = "";
+    } else {
+      token += str[i];
+    }
+    i++;
+  }
+  result.push(token);
+
+  return result;
+}
+console.log(split(str, " "));
